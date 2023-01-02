9ee0a8f204e9e149856c3e22618405a56b31407c
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2023-01-02 20:11:20 -0300

    add auth

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const allowedOrigins = process.env.IP_LIST.split(',') || '';
+  const app = await NestFactory.create(AppModule, { cors: true  });
+
+  app.useGlobalPipes(new ValidationPipe())
 
   const config = new DocumentBuilder()
   .setTitle('Social Media Server')
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,37 +1,61 @@
-import { Body, Get, Param, Patch, Post } from "@nestjs/common/decorators";
-import { LoggedUser } from "src/utils/logged-user.decorator";
-import { CreateUserDto } from "./dto/create-user.dto";
-import { UpdateUserDto } from "./dto/update-user.dto";
-import { UserEntity } from "./entities/user.entity";
-import { UsersService } from "./users.service";
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common/decorators';
+import { Delete } from '@nestjs/common/decorators/http/request-mapping.decorator';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { LoggedUser } from 'src/utils/logged-user.decorator';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserEntity } from './entities/user.entity';
+import { UsersService } from './users.service';
 
-export class UsersController{
-    constructor(private readonly usersService: UsersService){}
+@ApiTags('user')
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  @ApiOperation({
+    summary: 'Create a new User',
+  })
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.createUser(createUserDto);
+  }
 
-    @Post()
-    createUser(@Body() createUserDto: CreateUserDto){
-        return this.usersService.createUser(createUserDto);
-    }
+  @Get()
+  findAllUsers() {
+    return this.usersService.findAllUsers();
+  }
 
-    @Get()
-    findAllUsers() {
-        return this.usersService.findAllUsers();
-    }
+  @Get(':id or name')
+  @ApiOperation({
+    summary: 'Get a User by ID',
+  })
+  findUserById(@Param('id') id: string, email: string) {
+    return this.usersService.findUserById(id, email);
+  }
 
-    @Get()
-    findUserById(@Param('id') id:string, email:string){
-        return this.usersService.findUserById(id, email);
-    }
+  @Patch('id')
+  @ApiOperation({
+    summary: 'Use to update partial or total a User by ID',
+  })
+  updateUser(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+    @LoggedUser() user: UserEntity,
+  ) {
+    return this.usersService.updateUser(id, updateUserDto, user);
+  }
 
-    @Patch()
-    updateUser(
-        @Param('id') id: string,
-        @Body() updateUserDto: UpdateUserDto,
-        @LoggedUser() user: UserEntity,
-      ) {
-        return this.usersService.updateUser(id, updateUserDto, user);
-      }
-
-    removeUser
-
+  @Delete(':id')
+  @ApiOperation({
+    summary: 'Remove a User by ID',
+  })
+  removeUser(@Param('id') id: string, @LoggedUser() user: UserEntity) {
+    return this.usersService.removeUser(id, user);
+  }
 }
