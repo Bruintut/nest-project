2f3c998454a6f37f3b97b6900e70bd91d0617436
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2023-01-02 21:07:57 -0300

    add tags swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateUserDto{
-    
+    @ApiProperty({
+        description: 'User`s name address',
+        example: 'email@emailcom.br',
+      })
     name: string;
+
+    @ApiProperty({
+        description: 'User`s email address',
+        example: 'email@emailcom.br',
+      })
     email: string;
+
+    @ApiProperty({
+        description: 'User`s password address',
+        example: 'senha2022',
+      })
     password: string;
 }
