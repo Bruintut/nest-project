066266819548be7b182dea78b81f4fb0cd2a1c36
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2023-01-13 13:47:19 -0300

    prisma migrate

--- prisma/migrations/20230113164631_initial/migration.sql
+++ prisma/migrations/20230113164631_initial/migration.sql
@@ -1,11 +1,13 @@
 -- CreateTable
 CREATE TABLE "users" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL,
-    "updatedAt" DATETIME NOT NULL
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
